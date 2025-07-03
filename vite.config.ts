d5d1f94801f5d7a1c4c65385c5f6d43362105256
Gunxzq <3219988985@qq.com>
2025-07-03 15:14:54 +0800

    build(vite): 优化项目配置和构建设置
    - 更新 tsconfig.app.json，增加 typeRoots 和 paths 配置
    - 简化 tsconfig.json 结构
    - 重构 vite.config.ts，优化路径配置和构建选项
    - 添加代码压缩、混淆和注释移除等构建优化

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { build, defineConfig } from 'vite';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 import vuetify from 'vite-plugin-vuetify';
-// https://vite.dev/config/
-export default defineConfig((command, mode) => {
+export default defineConfig((env: any) => {
   // const env = loadEnv(mode, process.cwd());
+  const mode = env.mode;
+  // const command = env.command;
   return {
-    resolve: {
-      '@': path.resolve('./src'),
+    alias: {
+      '@': path.resolve(__dirname, '.src'),
     },
     plugins: [
       vue(),
@@ -19,9 +20,39 @@ export default defineConfig((command, mode) => {
     server: {
       port: 5173,
     },
+
+    optimizedDependencies: {
+      csscodesplit: true,
+      entries: ['src/main.ts'],
+      include: ['vue', 'vue-router', 'pinia', 'vuetify'],
+    },
     build: {
       outDir: 'dist',
       SourceMap: true,
+      minify: mode == 'development' ? false : 'terser',
+      // minify: 'terser',
+      terserOptions: {
+        compress: {
+          // 移除console
+          drop_console: mode == 'development' ? false : true,
+          // drop_console: true,
+          drop_debugger: true, // 移除 debugger
+        },
+        format: {
+          comments: false, // 移除注释
+        },
+        mangle: true, // 混淆变量名
+      },
+      rollupOptions: {
+        output: {
+          // 设置缓存头
+          manualChunks(id: any) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+        },
+      },
     },
   };
 });
